d09301d2a1140662cd5aee1815fd3278abb5844c
Michael Chan <michael@Michaels-MacBook-Pro.local>
2022-12-19 09:44:28 -0500

    Created the four algebraic functions for calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+let num1 = 2;
+let num2 = 2;
+console.log(add(num1, num2));
+console.log(subtract(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
