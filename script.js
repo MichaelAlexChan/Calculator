5aefac3d04fa7e455f58be181aa81f1c12ee6497
Michael Chan <michael@Michaels-MacBook-Pro.local>
2022-12-19 10:04:54 -0500

    Create an operate function to perform an algebraic  operation on two integers

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
+const operate = (string, num1, num2) => {
+    let value;
+    switch(string) {
+        case "+":
+            value = add(num1, num2);
+            break;
+        case "-":
+            value = subtract(num1, num2);
+            break;
+        case "*":
+            value = multiply(num1, num2);
+            break;
+        case "/":
+            value = divide(num1, num2);
+            break;
+        default:
+            console.log("Are u dumbbb fammm");
+    }
+    return value;
+}
+
+let operator = "*";
 let num1 = 2;
 let num2 = 2;
-console.log(add(num1, num2));
-console.log(subtract(num1, num2));
-console.log(multiply(num1, num2));
-console.log(divide(num1, num2));
+
+console.log(operate(operator, num1, num2));
+
