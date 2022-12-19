42bd30a2162278f3a84bb040e52af4d7838b26fd
Michael Chan <michael@Michaels-MacBook-Pro.local>
2022-12-19 10:20:33 -0500

    Commented for additional work

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
-
+/*
+    Things to work on: 
+    -division by 0;
+    -floating numbers
+*/
 const operate = (string, num1, num2) => {
     let value;
     switch(string) {
