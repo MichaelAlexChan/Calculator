59d937a334dc63028a2d0791269fdc43741b7b09
Michael Chan <michael@Michaels-MacBook-Pro.local>
2022-12-21 11:40:55 -0500

    Changed the calculator grid format, added new styles and ids, added js event listeners to buttons

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const divide = (num1, num2) => num1 / num2;
     -division by 0;
     -floating numbers
 */
+
 const operate = (string, num1, num2) => {
     let value;
     switch(string) {
@@ -28,9 +29,21 @@ const operate = (string, num1, num2) => {
     return value;
 }
 
-let operator = "*";
-let num1 = 2;
-let num2 = 2;
+const buttons = document.getElementById('grid-container');
+const display = document.getElementById('numberDisplay');
 
-console.log(operate(operator, num1, num2));
+let num1;
+let num2;
+let operator;
+
+buttons.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+
+    if (!isButton) {
+        return;
+    }
+    
+    console.log(typeof(event.target.innerText));
+    display.textContent += event.target.innerText;
+});
 
